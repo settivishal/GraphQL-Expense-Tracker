eec03911508fbda3f56939ac04ea474fb2125585
settivishal <vishalsetti2000@gmail.com>
2025-03-16 14:25:28 -0400

    passport implementation done

--- backend/db/connectDB.js
+++ backend/db/connectDB.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
+const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
@@ -9,3 +9,5 @@ export const connectDB = async () => {
     process.exit(1);
   }
 };
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,54 @@
 import express from "express";
 import http from "http";
 import cors from "cors";
-import dotenv from "dotenv";
+import dotenv, { config } from "dotenv";
+
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
 
 import { ApolloServer } from "@apollo/server";
 import { expressMiddleware } from "@apollo/server/express4";
 import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { buildContext } from "graphql-passport";
 
 import mergedTypeDefs from "./typeDefs/index.js";
 import mergedResolvers from "./resolvers/index.js";
-import { connectDB } from "./db/connectDB.js";
+
+import connectDB from "./db/connectDB.js";
+import configurePassport from "./passport/passport.config.js";
 
 dotenv.config();
+configurePassport();
 
 const app = express();
+
 const httpServer = http.createServer(app);
+
+const MongoStore = connectMongo(session);
+
+const store = new MongoStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", (err) => console.log(err));
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+      httpOnly: true,
+    },
+    store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 const server = new ApolloServer({
   typeDefs: mergedTypeDefs,
@@ -26,10 +60,13 @@ await server.start();
 
 app.use(
   "/",
-  cors(),
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
   express.json(),
   expressMiddleware(server, {
-    context: async ({ req }) => ({ req }),
+    context: async ({ req, res }) => buildContext({ req, res }),
   })
 );
 
